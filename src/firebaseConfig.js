2872923f92b956c190e27aecfc6d2fa1676db928
Rodrigo Moscoso <rodrigo.moscoso@gmail.com>
2022-09-30 10:34:17 -0400

    Se configuró la autenticación con Google

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlVEqQ6XR2HXaVwvSL1I6Lzlo5WnmJ5c0",
+  authDomain: "task-list-proyecto-ada-2022.firebaseapp.com",
+  projectId: "task-list-proyecto-ada-2022",
+  storageBucket: "task-list-proyecto-ada-2022.appspot.com",
+  messagingSenderId: "285678639441",
+  appId: "1:285678639441:web:b61323606923cba5a7db70",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,24 @@
-import { Heading, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Heading, HStack, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { app } from "../firebaseConfig";
 
 export default function Home() {
+  const [errroMessage, setErrorMessage] = useState("");
+
+  const handleGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+
+    signInWithPopup(auth, provider)
+      .then((userCredential) => {
+        console.log(userCredential.user.displayName);
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
   return (
     <VStack alignContent="center">
       <Heading
@@ -16,21 +34,41 @@ export default function Home() {
       >
         Home
       </Heading>
-      <Text
-        boxSize="70%"
-        fontSize="xl"
-        textAlign="center"
-        color="pink.500"
-        borderWidth="2px"
-        borderRadius="md"
-        borderRightColor="pink.600"
-        borderBottomColor="pink.600"
-        p="3%"
-      >
-        Bienvenido a la aplicación para gestionar tareas. Donde podrá hacer un
-        seguimiento a las actividades que estén pendientes, en proceso o
-        completadas.
-      </Text>
+      <HStack justify="center">
+        <Text
+          boxSize="50%"
+          fontSize="xl"
+          textAlign="center"
+          color="pink.500"
+          borderWidth="2px"
+          borderRadius="md"
+          borderRightColor="pink.600"
+          borderBottomColor="pink.600"
+          p="3%"
+        >
+          Bienvenido a la aplicación para gestionar tareas. Donde podrá hacer un
+          seguimiento a las actividades que estén pendientes, en proceso o
+          completadas.
+        </Text>
+        <VStack
+          boxSize="30%"
+          fontSize="xl"
+          textAlign="center"
+          color="pink.500"
+          borderWidth="2px"
+          borderRadius="md"
+          borderRightColor="pink.600"
+          borderBottomColor="pink.600"
+          p="3%"
+          spacing="5%"
+        >
+          <Text as="b" fontSize="xl" bold textAlign="center" color="pink.500">
+            Ingrese a la Aplicación:
+          </Text>
+          <Button onClick={handleGoogle}>Registrarse con Google</Button>
+          {errroMessage && <p>{errroMessage}</p>}
+        </VStack>
+      </HStack>
     </VStack>
   );
 }
