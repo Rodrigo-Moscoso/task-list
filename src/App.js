28839508937e929d0fa23c51fb1b8db545768138
Rodrigo Moscoso <rodrigo.moscoso@gmail.com>
2022-10-04 16:48:09 -0400

    Se icluyo el modo oscuro a esta rama, solo estaba en la rama review-3

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./views/Home";
 import SobreNosotros from "./views/SobreNosotros";
 import Tareas from "./views/Tareas";
-import { Button, Center, HStack, Text } from "@chakra-ui/react";
+import { Button, Center, HStack, Text, useColorMode } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 import { useUser } from "./customHooks/useUser.jsx";
 import Footer from "./components/Footer";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
   const { user } = useUser();
   return (
     <>
       <Header />
+
+      <HStack justify="right">
+        <Button onClick={toggleColorMode} marginRight="20px" marginTop="10px">
+          {colorMode === "dark" ? (
+            <SunIcon color="orange.200" />
+          ) : (
+            <MoonIcon color="blue.700" />
+          )}
+        </Button>
+      </HStack>
 
       <Router>
         <HStack p="3%" justify="center" spacing="3%">
